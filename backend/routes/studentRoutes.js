e647733a19ea26c4a4fa4e8150d58bc50e6b2f96
M-Talha183 <ullahehsan016@gmail.com>
2026-07-04 21:56:15 +0500

    add  successfully dynamic routes

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { addstudent , getStudents} from "../controllers/studentController.js";
+import { addstudent , getStudents ,getStudentById} from "../controllers/studentController.js";
 
 const router = express.Router();
 
 router.post("/add-student", addstudent);
 router.get("/getStudents", getStudents);
-
+router.get("/getStudentById/:id", getStudentById);
 export default router;
