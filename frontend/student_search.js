f7b4c9cd0974de9340b14f4a25238f7998a4d69a
M-Talha183 <ullahehsan016@gmail.com>
2026-07-19 22:41:29 +0500

    Add some new code

--- frontend/student_search.js
+++ frontend/student_search.js
@@ -1,1 +1,19 @@
-// code fir student search
+const searchForm = document.getElementById('searchForm');
+const searchQuery = document.getElementById('searchQuery');
+
+searchForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const query = searchQuery.value.trim();
+
+
+    try {
+        const response = await fetch(
+            `http://localhost:5000/api/students/searchStudents?query=${query}`
+        );
+        const students = await response.json();
+        console.log(students);
+    } catch (error) {
+        console.error('Error searching students:', error);
+    }
+    
+});
