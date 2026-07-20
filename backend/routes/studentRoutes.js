2d5c5cf7d2eb44b70a0b790bdb7d1627abe49fe0
M-Talha183 <ullahehsan016@gmail.com>
2026-07-20 22:44:11 +0500

    update the search studdent by id code

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { addstudent , getStudents ,getStudentById} from "../controllers/studentController.js";
+import { addstudent , getStudents ,getStudentByIdOrName} from "../controllers/studentController.js";
 
 const router = express.Router();
 
 router.post("/add-student", addstudent);
 router.get("/getStudents", getStudents);
-router.get("/getStudentById/:id", getStudentById);
+// router.get("/getStudentById/:id", getStudentById);
+router.get('/search', getStudentByIdOrName);
+
 export default router;
